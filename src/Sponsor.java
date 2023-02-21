cc0b4f7df172f59baebca3c51baa62cde83d9b9d
thee freelancer <ronaldssebalamu2@gmail.com>
2023-02-21 04:51:56 +0300

    Made changes to the Sponsor file

--- src/Sponsor.java
+++ src/Sponsor.java
@@ -5,54 +5,110 @@
 // (powered by FernFlower decompiler)
 //
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Sponsor {
-    static float bankAccount = 1300.0F;
-    static float[] tuitionFares = new float[]{50.0F, 30.0F, 20.0F, 10.0F, 20.0F, 30.0F, 40.0F, 30.0F, 20.0F, 10.0F, 20.0F, 30.0F, 40.0F, 30.0F};
-
+    static double bankAccount = 1300.0D;
     public Sponsor() {
     }
 
-    public static void main(String[] var0) {
-        String[] var1 = new String[]{"Ronald", "Vivian", "Joan", "Vickie", "kennedy", "Joseph", "Julius", "Junior", "Irene", "Winnie", "Dorothy", "Flora", "Rhonah", "Nsubuga"};
-        Scanner var2 = new Scanner(System.in);
-        String var3 = var2.nextLine().toLowerCase();
-        if (var3.equals("leave")) {
-            System.out.println("Thanks for visiting our school payment platform.We shall be waiting for you when you come!");
-        } else {
-            boolean var4 = false;
+    public static void main(String[] args) {
+//      Create a HashMap, to store an individual student's name and the amount of tuition a given student is supposed to pay.
+        HashMap<String, Double> studentFares = new HashMap<>();
+        studentFares.put("Ronald", 50.0D);
+        studentFares.put("Vivian", 30.0D);
+        studentFares.put("Joan", 20.0D);
+        studentFares.put("Vickie", 10.0D);
+        studentFares.put("Kennedy", 20.0D);
+        studentFares.put("Joseph", 30.0D);
+        studentFares.put("Julius", 40.0D);
+        studentFares.put("Junior", 12.0D);
+        studentFares.put("Irene", 8.0D);
+        studentFares.put("Winnie", 15.0D);
+        studentFares.put("Dorothy", 10.0D);
+        studentFares.put("Flora", 10.0D);
+        studentFares.put("Rhona", 12.0D);
+        studentFares.put("Nsubuga", 18.0D);
 
-            int var5;
-            for(var5 = 0; var5 > var1.length; ++var5) {
-                if (var3.equalsIgnoreCase(var1[var5])) {
-                    var4 = true;
+//      Calls the scanner utility class to prompt the user to input a command that is either "payments" or "leave"
+        Scanner originalInput = new Scanner(System.in);
+        System.out.println("Enter prompt:");
+        String input = originalInput.nextLine().toLowerCase();
+
+//       Performs a check if the user actually entered a command called "Payments" and the input shouldn't be null.
+        if (input !=null && input.equals("payments")) {
+
+//      Calls the user to input the name of the enrolled student
+            Scanner studentPrompt = new Scanner(System.in);
+            System.out.println("Enter enrolled name:");
+            String studentName = studentPrompt.nextLine();
+            boolean isStudentEnrolled = false;
+
+            for (String entry : studentFares.keySet()) {
+                if (entry.equals(studentName)) {
+//              Equates the existing entry to the prompted studentName
+                    isStudentEnrolled = true;
                     break;
                 }
             }
+            if (isStudentEnrolled) {
 
-            if (var4) {
-                for(var5 = 0; var5 < tuitionFares.length; ++var5) {
-                    float var10001 = tuitionFares[var5];
-                    System.out.println("This student enrolled in our school and he paid" + var10001 + "Would you like to check his proof of Payment");
-                    String var6 = var2.nextLine().toLowerCase();
-                    if (!var6.equals("yes") && !var6.equals("y")) {
-                        System.out.println("We are deeply Sorry, We have no student that goes by those names in our school, Try somewhere else.");
-                        String var7 = var2.nextLine().toLowerCase();
-                        if (!var7.equals("no") && !var7.equals("n")) {
-                            System.out.println("You haven't pressed anything, Please try again!");
-                        } else {
-                            System.out.println("Thank you. We shall always be here waiting to work on you when you are ready.");
-                        }
-                    } else if (bankAccount >= tuitionFares[var5]) {
-                        System.out.println("Congratulations, Thanks for paying our student's tuition for this semester, Looking forward to seeing him.");
-                        bankAccount -= tuitionFares[var5];
-                    } else if (bankAccount < tuitionFares[var5]) {
-                        System.out.println("Sorry, we don't have enough funds to pay for your student's tuition");
+//              Performs an action when the user's name is in the list of the enrolled students
+                System.out.println("The student " + entry.getKey() + "pays" + entry.getValue());
+
+//              Prompts the sponsor to make a choice on whether to make a payment for the beneficiary or not.
+                Scanner paymentPrompt = new Scanner(System.in);
+                System.out.println("Should we make payments? ");
+                String paymentCmd = paymentPrompt.nextLine();
+                if(paymentCmd.equals("yes") || paymentCmd.equals("y")){
+                    if(bankAccount >= entry.getValue()){
+                        System.out.println("Thank you for making this payment!");
+                        bankAccount--;
+                    }else if (bankAccount < entry.getValue()){
+                        System.out.println("sorry, the account balance on your account can support this transaction");
                     }
+                } else if (paymentCmd.equals("no") || paymentCmd.equals("n")){
+                    System.out.println("Thank you for choosing us for all your payment services, you can as well try again later!");
+
                 }
+            } else {
+                System.out.println("A student with those details did not enroll in our institution");
             }
+
+        } else if(input.equals("quit") || input.equals("q")) {
+            System.out.println("Thanks for visiting our school payment platform.We shall be waiting for you when you come!");
+
         }
 
     }
 }
+//            for(counter = 0; counter < students.length; counter++) {
+//                if (input.equalsIgnoreCase(students[counter])) {
+//                    isStudentEnrolled = true;
+//                    break;
+//                }
+//            }
+//
+//            if (isStudentEnrolled) {
+//                for(counter = 0; counter < tuitionFares.length; counter++) {
+//                    float payment = tuitionFares[counter];
+//                    System.out.println("This student enrolled in our school and he paid" + payment + "Would you like to check his proof of Payment");
+//                    String userPrompt = originalInput.nextLine().toLowerCase();
+//                    if (!userPrompt.equals("yes") && !userPrompt.equals("y")) {
+//                        System.out.println("We are deeply Sorry, We have no student that goes by those names in our school, Try somewhere else.");
+//                        String userPrompt1 = originalInput.nextLine().toLowerCase();
+//                        if (!userPrompt1.equals("no") && !userPrompt1.equals("n")) {
+//                            System.out.println("Thank you for choosing our payment system, we are always looking forward to serving you.");
+//                        } else {
+//                            System.out.println("Thank you. We shall always be here waiting to work on you when you are ready.");
+//                        }
+//                    } else if (bankAccount >= tuitionFares[counter]) {
+//                        System.out.println("Congratulations, Thanks for paying our student's tuition for this semester, Looking forward to seeing him.");
+//                        bankAccount -= tuitionFares[counter];
+//                    } else if (bankAccount < tuitionFares[counter]) {
+//                        System.out.println("Sorry, we don't have enough funds to pay for your student's tuition");
+//                    }
+//                }
+//            }
+//        }
